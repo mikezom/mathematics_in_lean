af94296ba2794ea8ab2db0de7bf6ec97063cc6a0
mikezom <mikezom3721@gmail.com>
2024-11-05 17:05:15 +0800

    Now on C02S02

--- MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
+++ MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
@@ -53,13 +53,14 @@ theorem neg_add_cancel_left (a b : R) : -a + (a + b) = b := by
 
 -- Prove these:
 theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
-  sorry
+  rw [add_assoc, MyRing.add_right_neg b, add_zero]
 
 theorem add_left_cancel {a b c : R} (h : a + b = a + c) : b = c := by
-  sorry
+  rw [<-neg_add_cancel_left a b, h, neg_add_cancel_left a c]
 
 theorem add_right_cancel {a b c : R} (h : a + b = c + b) : a = c := by
-  sorry
+  rw [add_comm a b, add_comm c b] at h
+  exact add_left_cancel h
 
 theorem mul_zero (a : R) : a * 0 = 0 := by
   have h : a * 0 + a * 0 = a * 0 + 0 := by
@@ -67,20 +68,27 @@ theorem mul_zero (a : R) : a * 0 = 0 := by
   rw [add_left_cancel h]
 
 theorem zero_mul (a : R) : 0 * a = 0 := by
-  sorry
+  have h : 0 * a + 0 * a = 0 * a + 0 := by
+    rw [<- add_mul, zero_add, add_zero]
+  rw [add_left_cancel h]
 
 theorem neg_eq_of_add_eq_zero {a b : R} (h : a + b = 0) : -a = b := by
-  sorry
+  have h: -a + a = b + a := by
+    rw [neg_add_cancel, add_comm, h]
+  apply add_right_cancel h
 
 theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
-  sorry
+  have h: a + b = -b + b := by
+    rw [h, neg_add_cancel]
+  apply add_right_cancel h
 
 theorem neg_zero : (-0 : R) = 0 := by
   apply neg_eq_of_add_eq_zero
   rw [add_zero]
 
 theorem neg_neg (a : R) : - -a = a := by
-  sorry
+  apply neg_eq_of_add_eq_zero
+  rw [neg_add_cancel]
 
 end MyRing
 
@@ -103,13 +111,13 @@ namespace MyRing
 variable {R : Type*} [Ring R]
 
 theorem self_sub (a : R) : a - a = 0 := by
-  sorry
+  rw [sub_eq_add_neg, add_neg_cancel]
 
 theorem one_add_one_eq_two : 1 + 1 = (2 : R) := by
   norm_num
 
 theorem two_mul (a : R) : 2 * a = a + a := by
-  sorry
+  rw [<-one_add_one_eq_two, add_mul, one_mul]
 
 end MyRing
 
@@ -131,16 +139,25 @@ variable {G : Type*} [Group G]
 
 namespace MyGroup
 
+lemma mul_right_eq_one {a b : G} (h : a * b = a) : b = 1 := by
+  rw [<- inv_mul_cancel a, <- one_mul b, <- inv_mul_cancel a, mul_assoc, h]
+
 theorem mul_inv_cancel (a : G) : a * a⁻¹ = 1 := by
+  have h : (a * a⁻¹) * (a * a⁻¹) = (a * a⁻¹) := by
+    rw [mul_assoc, <- mul_assoc a⁻¹, inv_mul_cancel, one_mul]
+  apply mul_right_eq_one h
+
+lemma mul_right_cancel {a b c : G} (h : a * b = c * b) : a = c := by
   sorry
 
 theorem mul_one (a : G) : a * 1 = a := by
-  sorry
+  have h : a * 1 * a⁻¹ = a * a⁻¹ := by
+    rw [mul_inv_cancel, mul_assoc, one_mul, mul_inv_cancel]
+  apply mul_right_cancel h
 
 theorem mul_inv_rev (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
-  sorry
+  group
 
 end MyGroup
 
 end
-
